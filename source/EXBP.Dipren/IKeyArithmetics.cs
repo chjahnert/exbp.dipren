ef4e336d8730dc9b03a48ac64575a90f42b588e5
Christian Jähnert <ch.jahnert@gmail.com>
2022-07-17 23:57:53 +0200

    Defines the IKeyArithmethics<TKey> interface.

---
+++ source/EXBP.Dipren/IKeyArithmetics.cs
@@ -0,0 +1,23 @@
+﻿
+namespace EXBP.Dipren
+{
+    /// <summary>
+    ///   Implements operations for manipulating key ranges.
+    /// </summary>
+    /// <typeparam name="TKey">
+    ///   The type of keys.
+    /// </typeparam>
+    public interface IKeyArithmetics<TKey> where TKey : IComparable<TKey>
+    {
+        /// <summary>
+        ///   Splits the specified range into two ranges.
+        /// </summary>
+        /// <param name="range">
+        ///   The <see cref="Range{TKey}"/> to split.
+        /// </param>
+        /// <returns>
+        ///   An array of <see cref="Range{TKey}"/> objects.
+        /// </returns>
+        Range<TKey>[] Split(Range<TKey> range);
+    }
+}
