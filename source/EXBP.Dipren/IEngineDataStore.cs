c18706ef8e687e71b0a56b15d332b6715a032094
Christian Jähnert <ch.jahnert@gmail.com>
2022-07-18 19:41:27 +0200

    Introduces the IEngineDataStore interface.

---
+++ source/EXBP.Dipren/IEngineDataStore.cs
@@ -0,0 +1,10 @@
+﻿
+namespace EXBP.Dipren
+{
+    /// <summary>
+    ///   Allows a class to implement a data store for the distributed processing engine.
+    /// </summary>
+    public interface IEngineDataStore
+    {
+    }
+}
