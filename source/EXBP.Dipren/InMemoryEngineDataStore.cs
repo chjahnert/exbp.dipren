db02b1231b105eeadc8aba6b647818c322945c03
Christian Jähnert <ch.jahnert@gmail.com>
2022-07-19 15:16:13 +0200

    Introduces the InMemoryEngineDataStore class.

---
+++ source/EXBP.Dipren/InMemoryEngineDataStore.cs
@@ -0,0 +1,16 @@
+﻿
+namespace EXBP.Dipren
+{
+    /// <summary>
+    ///   Implements an in-memory <see cref="IEngineDataStore"/> that can be used for testing.
+    /// </summary>
+    public class InMemoryEngineDataStore
+    {
+        /// <summary>
+        ///   Initializes a new and empty instance of the <see cref="InMemoryEngineDataStore"/> class.
+        /// </summary>
+        public InMemoryEngineDataStore()
+        {
+        }
+    }
+}
