3450eeb08f2cdc2d7487d0bcaf84b2c2a93b2a2d
Christian Jähnert <ch.jahnert@gmail.com>
2022-07-19 15:24:51 +0200

    Introduces the InMemoryEngineDataStore.Job type.

--- source/EXBP.Dipren/InMemoryEngineDataStore.cs
+++ source/EXBP.Dipren/InMemoryEngineDataStore.cs
@@ -12,5 +12,44 @@ namespace EXBP.Dipren
         public InMemoryEngineDataStore()
         {
         }
+
+
+        /// <summary>
+        ///   Represents a distributed processing job in an <see cref="InMemoryEngineDataStore"/> instance.
+        /// </summary>
+        private sealed class Job
+        {
+            /// <summary>
+            ///   Gets the unique name of the current distributed processing job.
+            /// </summary>
+            /// <value>
+            ///   A <see cref="string"/> value containing the unique name of the current distributed processing job.
+            /// </value>
+            public string Name { get; }
+
+            /// <summary>
+            ///   Gets or sets a value that indicates the state of the current distributed processing job.
+            /// </summary>
+            /// <value>
+            ///   A <see cref="JobState"/> value that indicates the state of the current distributed processing job.
+            /// </value>
+            public JobState State { get; set; }
+
+
+            /// <summary>
+            ///   Initializes a new instance of the <see cref="Job"/> class.
+            /// </summary>
+            /// <param name="name">
+            ///   The unique name of the job.
+            /// </param>
+            /// <param name="state">
+            ///   The initial state of the job.
+            /// </param>
+            public Job(string name, JobState state)
+            {
+                this.Name = name;
+                this.State = state;
+            }   
+        }
     }
 }
