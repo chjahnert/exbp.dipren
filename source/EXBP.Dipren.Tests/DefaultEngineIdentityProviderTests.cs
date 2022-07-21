692978957278ba59e4d45f41f7a7e299ee4781a6
Christian Jähnert <ch.jahnert@gmail.com>
2022-07-21 11:09:17 +0200

    Introduces the DefaultEngineIdentityProvider type.

---
+++ source/EXBP.Dipren.Tests/DefaultEngineIdentityProviderTests.cs
@@ -0,0 +1,22 @@
+﻿
+using NUnit.Framework;
+
+
+namespace EXBP.Dipren.Tests
+{
+    [TestFixture]
+    internal class DefaultEngineIdentityProviderTests
+    {
+        [Test]
+        public void GetEngineIdentifier_InstanceCreated_ReturnsExpecedIdentifier()
+        {
+            IEngineIdentityProvider provider = new DefaultEngineIdentityProvider();
+
+            string actual = provider.GetEngineIdentifier();
+
+            string expected = FormattableString.Invariant($"{Environment.MachineName}{DefaultEngineIdentityProvider.DELIMITER}{Environment.ProcessId}{DefaultEngineIdentityProvider.DELIMITER}{AppDomain.CurrentDomain.Id}{DefaultEngineIdentityProvider.DELIMITER}");
+
+            StringAssert.StartsWith(expected, actual);
+        }
+    }
+}
