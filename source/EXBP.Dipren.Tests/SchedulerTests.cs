96721bd69bfaa4dcd4fb0c54d10c921dd57447b8
Christian Jähnert <ch.jahnert@gmail.com>
2022-08-19 12:09:32 +0200

    Implements unit tests covering the constructors of the Scheduler type.

---
+++ source/EXBP.Dipren.Tests/SchedulerTests.cs
@@ -0,0 +1,28 @@
+﻿
+using EXBP.Dipren.Data;
+
+using NSubstitute;
+
+using NUnit.Framework;
+
+
+namespace EXBP.Dipren.Tests
+{
+    [TestFixture]
+    public class SchedulerTests
+    {
+        [Test]
+        public void Ctor_ArgumentStoreIsNull_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Scheduler(null));
+        }
+
+        [Test]
+        public void Ctor_ArgumentClockIsNull_ThrowsException()
+        {
+            IEngineDataStore store = Substitute.For<IEngineDataStore>();
+
+            Assert.Throws<ArgumentNullException>(() => new Scheduler(store, null));
+        }
+    }
+}
