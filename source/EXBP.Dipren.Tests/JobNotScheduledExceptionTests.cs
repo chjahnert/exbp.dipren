f180935838125ee49655155c38015721f481cb5c
Christian Jähnert <chj@cludo.com>
2022-09-21 15:47:47 +0200

    Introduces the JobNotScheduledException type.

---
+++ source/EXBP.Dipren.Tests/JobNotScheduledExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿
+using NUnit.Framework;
+
+
+namespace EXBP.Dipren.Tests
+{
+    [TestFixture]
+    public class JobNotScheduledExceptionTests
+    {
+        [Test]
+        public void Serialization_ValidInstance_IsRestored()
+        {
+            const string id = "DPJ-0001";
+
+            JobNotScheduledException exception = new JobNotScheduledException("Message", id);
+            JobNotScheduledException restored = SerializationUtilities.Serialize(exception);
+
+            Assert.That(restored, Is.Not.Null);
+            Assert.That(restored.Id, Is.EqualTo(id));
+        }
+    }
+}
