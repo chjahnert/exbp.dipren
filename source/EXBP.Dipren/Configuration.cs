9508fbf66022be446110e6181b2d0347ae1a9c55
Christian Jähnert <chj@cludo.com>
2022-09-23 09:28:41 +0200

    Introduces the Configuration.BatchProcessingTimeout property.

--- source/EXBP.Dipren/Configuration.cs
+++ source/EXBP.Dipren/Configuration.cs
@@ -12,10 +12,12 @@ namespace EXBP.Dipren
     {
         private const double DEFAULT_MAXIMUM_CLOCK_DRIFT_SECONDS = 2.0;
         private const double DEFAULT_POLLING_INTERVAL_SECONDS = 2.0;
+        private const double DEFAULT_BATCH_PROCESSING_TIMEOUT_SECONDS = 60.0;
 
 
         private readonly TimeSpan _maximumClockDrift = TimeSpan.FromSeconds(DEFAULT_MAXIMUM_CLOCK_DRIFT_SECONDS);
         private readonly TimeSpan _pollingInterval = TimeSpan.FromSeconds(DEFAULT_POLLING_INTERVAL_SECONDS);
+        private readonly TimeSpan _batchProcessingTimeout = TimeSpan.FromSeconds(DEFAULT_BATCH_PROCESSING_TIMEOUT_SECONDS);
 
 
         /// <summary>
@@ -56,6 +58,33 @@ namespace EXBP.Dipren
             }
         }
 
+        /// <summary>
+        ///   Gets the amount of time after which the processing of a batch is considered unsuccessful.
+        /// </summary>
+        /// <value>
+        ///   A <see cref="TimeSpan"/> value that indicates the amount of time after which the processing of a batch
+        ///   is considered unsuccessful.
+        /// </value>
+        /// <remarks>
+        ///   <para>
+        ///     When processing a batch takes longer than the specified timeout values, the operation is not canceled
+        ///     automatically. But if another processing node tries to acquire a partition and the partition was not
+        ///     updated for more than this timeout value, the partition is considered abandoned and might be taken by
+        ///     another processing node.
+        ///   </para>
+        /// </remarks>
+        public TimeSpan BatchProcessingTimeout
+        {
+            get
+            {
+                return this._batchProcessingTimeout;
+            }
+            init
+            {
+                this._batchProcessingTimeout = (value >= TimeSpan.Zero) ? value : (-1 * value);
+            }
+        }
+
 
         /// <summary>
         ///   Initializes a new instance of the <see cref="Configuration"/> class.
@@ -73,10 +102,14 @@ namespace EXBP.Dipren
         /// <param name="pollingInterval">
         ///   The interval at which changes are read from the engine data store.
         /// </param>
-        public Configuration(TimeSpan maximumClockDrift, TimeSpan pollingInterval)
+        /// <param name="batchProcessingTimeout">
+        ///   The amount of time after which the processing of a batch is considered unsuccessful.
+        /// </param>
+        public Configuration(TimeSpan maximumClockDrift, TimeSpan pollingInterval, TimeSpan batchProcessingTimeout)
         {
             this.MaximumClockDrift = maximumClockDrift;
             this.PollingInterval = pollingInterval;
+            this.BatchProcessingTimeout = batchProcessingTimeout;
         }
     }
 }
