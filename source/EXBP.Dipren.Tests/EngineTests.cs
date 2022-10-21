93a1eae9b3af71ba96b34c79e62bea35f4cc5e92
Christian Jähnert <chj@cludo.com>
2022-10-21 15:12:51 +0200

    Implements unit tests covering the argument validations of the Engine.RunAsync(...) method.

--- source/EXBP.Dipren.Tests/EngineTests.cs
+++ source/EXBP.Dipren.Tests/EngineTests.cs
@@ -1,4 +1,6 @@
 ﻿
+using EXBP.Dipren.Data.Memory;
+
 using NUnit.Framework;
 
 
@@ -11,6 +13,15 @@ namespace EXBP.Dipren.Tests
         public void Ctor_ArgumentStoreIsNull_ThrowsException()
         {
             Assert.Throws<ArgumentNullException>(() => new Engine(null));
+        }
+
+        [Test]
+        public void RunAsync_ArgumentJobIsNull_ThrowsException()
+        {
+            InMemoryEngineDataStore store = new InMemoryEngineDataStore();
+            Engine engine = new Engine(store);
+
+            Assert.ThrowsAsync<ArgumentNullException>(() => engine.RunAsync<int, int>(null, false, CancellationToken.None));
         }
     }
 }
