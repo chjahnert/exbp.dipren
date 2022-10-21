fc39aab6f76cea283181e120f82e8f07615feb20
Christian Jähnert <chj@cludo.com>
2022-10-21 15:11:46 +0200

    Implements unit tests covering the constructor of the Engine class.

---
+++ source/EXBP.Dipren.Tests/EngineTests.cs
@@ -0,0 +1,16 @@
+﻿
+using NUnit.Framework;
+
+
+namespace EXBP.Dipren.Tests
+{
+    [TestFixture]
+    public class EngineTests
+    {
+        [Test]
+        public void Ctor_ArgumentStoreIsNull_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Engine(null));
+        }
+    }
+}
