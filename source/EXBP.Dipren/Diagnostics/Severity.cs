cc40f9ac6331f5920587ac98cdd8975030dceccd
Christian Jähnert <chj@cludo.com>
2022-10-25 14:07:42 +0200

    Introduces the Severity enumeration.

---
+++ source/EXBP.Dipren/Diagnostics/Severity.cs
@@ -0,0 +1,24 @@
+﻿
+namespace EXBP.Dipren.Diagnostics
+{
+    /// <summary>
+    ///   Indicates the severity of a log message.
+    /// </summary>
+    public enum Severity
+    {
+        /// <summary>
+        ///   The log message provides details usually only needed for debugging purposes.
+        /// </summary>
+        Debug,
+
+        /// <summary>
+        ///   The log message provides details about a more important step and operation.
+        /// </summary>
+        Information,
+
+        /// <summary>
+        ///   The log message provides details about a error condition.
+        /// </summary>
+        Error
+    }
+}
