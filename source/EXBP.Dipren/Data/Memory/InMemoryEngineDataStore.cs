e54cc4097feff8c46da26ff0d8b67de14fb5f741
Christian Jähnert <chj@cludo.com>
2022-11-02 22:12:21 +0100

    Fixes the issue that the InMemoryEngineDataStore.TryRequestSplitAsync(...) method would include completed partitions in the list of candidates.

--- source/EXBP.Dipren/Data/Memory/InMemoryEngineDataStore.cs
+++ source/EXBP.Dipren/Data/Memory/InMemoryEngineDataStore.cs
@@ -481,7 +481,7 @@ namespace EXBP.Dipren.Data.Memory
                 }
 
                 Partition candidate = this._partitions
-                    .Where(p => (p.JobId == jobId) && (p.Owner != null) && (p.Updated >= active) && (p.Remaining > 0L) && (p.IsSplitRequested == false))
+                    .Where(p => (p.JobId == jobId) && (p.Owner != null) && (p.Updated >= active) && (p.IsCompleted == false) && (p.IsSplitRequested == false))
                     .OrderByDescending(p => p.Remaining)
                     .FirstOrDefault();
 
