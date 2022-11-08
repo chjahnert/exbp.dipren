2927279f464acef9f0fa04f6dffcf916bde40557
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-08 10:43:46 +0100

    Introduces the GuidLayout.DoNetFramework static property.

--- source/EXBP.Dipren/GuidLayout.cs
+++ source/EXBP.Dipren/GuidLayout.cs
@@ -26,6 +26,15 @@ namespace EXBP.Dipren
         /// </value>
         public static byte[] LexicographicalOrder { get; } = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
+        /// <summary>
+        ///   Gets byte layout of an GUID value used by .NET Framework.
+        /// </summary>
+        /// <value>
+        ///   An array of 16 <see cref="byte"/> values containing the layout for GUID values in little endian order
+        ///   used by the .NET Framework.
+        /// </value>
+        public static byte[] DoNetFramework { get; } = { 3, 2, 1, 0, 5, 4, 7, 6, 8, 9, 10, 11, 12, 13, 14, 15 };
+
 
         /// <summary>
         ///   Determines whether the specified <see cref="byte"/> array is a valid GUID layout.
