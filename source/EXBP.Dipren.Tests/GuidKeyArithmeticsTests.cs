d8f5dcb29b1a718aea95b17e0e9f6bdd29c08b5d
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-08 09:56:30 +0100

    Introduces the GuidLayout type.

---
+++ source/EXBP.Dipren.Tests/GuidKeyArithmeticsTests.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+
+namespace EXBP.Dipren.Tests
+{
+    [TestFixture]
+    public class GuidKeyArithmeticsTests
+    {
+        [Test]
+        public void Ctor_ArgumentLayoutIsNull_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GuidKeyArithmetics(null));
+        }
+
+        [TestCase(new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14} )]
+        [TestCase(new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 14} )]
+        [TestCase(new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 33} )]
+        public void Ctor_ArgumentLayoutIsNotValid_ThrowsException(byte[] layout)
+        {
+            Assert.Throws<ArgumentException>(() => new GuidKeyArithmetics(layout));
+        }
+    }
+}
---
+++ source/EXBP.Dipren/GuidLayout.cs
@@ -0,0 +1,54 @@
+﻿
+namespace EXBP.Dipren
+{
+    /// <summary>
+    ///   Defines well known GUID/UUID layouts in little endian order.
+    /// </summary>
+    public static class GuidLayout
+    {
+        private const int GUID_LENGTH = 16;
+
+
+        /// <summary>
+        ///   Gets byte layout of an UUID value used by Microsoft SQL Server.
+        /// </summary>
+        /// <value>
+        ///   An array of 16 <see cref="byte"/> values containing the layout for GUID values in little endian order
+        ///   used by Microsoft SQL Server.
+        /// </value>
+        public static byte[] MicrosoftSqlServer { get; } = { 10, 11, 12, 13, 14, 15, 8, 9, 6, 7, 4, 5, 0, 1, 2, 3 };
+
+
+        /// <summary>
+        ///   Determines whether the specified <see cref="byte"/> array is a valid GUID layout.
+        /// </summary>
+        /// <param name="layout">
+        ///   An array of 16 <see cref="byte"/> values between 0 and 15.
+        /// </param>
+        /// <returns>
+        ///   <see langword="true"/> if <see cref="layout"/> is valid; otherwise, <see langword="false"/>.
+        /// </returns>
+        internal static bool IsValid(byte[] layout)
+        {
+            bool result = false;
+
+            if (layout?.Length == GUID_LENGTH)
+            {
+                result = true;
+
+                List<byte> ordered = layout.OrderBy(x => x).ToList();
+
+                for (int i = 0; i < GUID_LENGTH; i++)
+                {
+                    if (ordered[i] != 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
