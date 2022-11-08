f40a4f9057d66b0e72525c8a14e3bf7dea4d71bb
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-08 16:17:08 +0100

    Implements additional unit tests covering the GuidKeyArithmetics.Split(...) method.

--- source/EXBP.Dipren.Tests/GuidKeyArithmeticsTests.cs
+++ source/EXBP.Dipren.Tests/GuidKeyArithmeticsTests.cs
@@ -50,5 +50,31 @@ namespace EXBP.Dipren.Tests
             yield return new object[] { GuidLayout.MicrosoftSqlServer, new Guid("00000000-0000-0000-0000-000000008000"), new Guid("00000000-0000-0000-0000-000000004000"), true, new Guid("00000000-0000-0000-0000-000000006000") };
             yield return new object[] { GuidLayout.MicrosoftSqlServer, new Guid("E0000000-0000-0000-0000-000000000000"), new Guid("A0000000-0000-0000-0000-000000000000"), false, new Guid("C0000000-0000-0000-0000-000000000000") };
         }
+
+        [TestCaseSource(nameof(Split_ArgumentRangeIsNotSplittable_ParameterSource))]
+        public void Split_ArgumentRangeIsNotSplittable_ReturnUnchangedRange(byte[] layout, Guid first, Guid last, bool inclusive)
+        {
+            Range<Guid> input = new Range<Guid>(first, last, inclusive);
+
+            GuidKeyArithmetics arithemtics = new GuidKeyArithmetics(layout);
+
+            Range<Guid> returned = arithemtics.Split(input, out Range<Guid> created);
+
+            Assert.That(returned.First, Is.EqualTo(input.First));
+            Assert.That(returned.Last, Is.EqualTo(input.Last));
+            Assert.That(returned.IsInclusive, Is.EqualTo(input.IsInclusive));
+
+            Assert.That(created, Is.Null);
+        }
+
+        public static IEnumerable Split_ArgumentRangeIsNotSplittable_ParameterSource()
+        {
+            yield return new object[] { GuidLayout.MicrosoftSqlServer, new Guid("04000000-0000-0000-0000-000000000000"), new Guid("04000000-0000-0000-0000-000000000000"), true };
+            yield return new object[] { GuidLayout.MicrosoftSqlServer, new Guid("04000000-0000-0000-0000-000000000000"), new Guid("05000000-0000-0000-0000-000000000000"), true };
+            yield return new object[] { GuidLayout.MicrosoftSqlServer, new Guid("04000000-0000-0000-0000-000000000000"), new Guid("06000000-0000-0000-0000-000000000000"), false };
+            yield return new object[] { GuidLayout.MicrosoftSqlServer, new Guid("04000000-0000-0000-0000-000000000000"), new Guid("04000000-0000-0000-0000-000000000000"), true };
+            yield return new object[] { GuidLayout.MicrosoftSqlServer, new Guid("05000000-0000-0000-0000-000000000000"), new Guid("04000000-0000-0000-0000-000000000000"), true };
+            yield return new object[] { GuidLayout.MicrosoftSqlServer, new Guid("06000000-0000-0000-0000-000000000000"), new Guid("04000000-0000-0000-0000-000000000000"), false };
+        }
     }
 }
