c189cbb27d1871f15dd217fd623db0165b17fb27
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-09 23:09:23 +0100

    Adds the EXBP.Dipren.Demo.Postgres console application project to the Dipren solution.

---
+++ demo/EXBP.Dipren.Demo.Postgres/EntryPoint.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EXBP.Dipren.Demo.Postgres
+{
+    internal class EntryPoint
+    {
+        internal static Task<int> Main(string[] args)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
