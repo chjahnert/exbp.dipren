62e3191cb8498446a968b12e9e4dda6316709e7f
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-10 13:34:02 +0100

    Implements the 'deploy' command.

--- demo/EXBP.Dipren.Demo.Postgres/Commands/Deploy.cs
+++ demo/EXBP.Dipren.Demo.Postgres/Commands/Deploy.cs
@@ -3,14 +3,36 @@ namespace EXBP.Dipren.Demo.Postgres.Commands
 {
     internal static class Deploy
     {
-        internal static Task<int> HandleAsync(string connectionString, int datasetSize)
+        internal static async Task<int> HandleAsync(string database, int size)
         {
-            Console.WriteLine("DEPLOY");
-            Console.WriteLine();
-            Console.WriteLine($"database: {connectionString}");
-            Console.WriteLine($"size:     {datasetSize}");
+            int result = 0;
 
-            return Task.FromResult(0);
+            try
+            {
+                Console.Write(DeployResources.MessageCreatingDemoSchema);
+                await Database.ExecuteNonQueryAsync(database, DeployQueries.DropDemoDatabaseSchema);
+                await Database.ExecuteNonQueryAsync(database, DeployQueries.CreateDemoDatabaseSchema);
+                Console.WriteLine(DeployResources.MessageDone);
+
+                Console.Write(DeployResources.MessageCreatingDiprenSchema);
+                await Database.ExecuteNonQueryAsync(database, DeployQueries.DropDiprenDatabaseSchema);
+                await Database.ExecuteNonQueryAsync(database, DeployQueries.CreateDiprenDatabaseSchema);
+                Console.WriteLine(DeployResources.MessageDone);
+
+                Console.Write(DeployResources.MessageGeneratingDimensions, size);
+                await Database.ExecuteNonQueryAsync(database, DeployQueries.GenerateDimensions, ("@limit", size));
+                Console.WriteLine(DeployResources.MessageDone);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(DeployResources.MessageFailed);
+                Console.WriteLine();
+                Console.WriteLine(ex);
+
+                result = -1;
+            }
+
+            return result;
         }
     }
 }
