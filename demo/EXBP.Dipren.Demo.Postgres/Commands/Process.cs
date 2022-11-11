1574b00f1670fda9fcabd34b3ec85cc12fe6c6ce
Christian Jähnert <16286828+chjahnert@users.noreply.github.com>
2022-11-11 23:00:17 +0100

    Fixes the name and byte order of the predefined UUID/GUID layouts (#43)

--- demo/EXBP.Dipren.Demo.Postgres/Commands/Process.cs
+++ demo/EXBP.Dipren.Demo.Postgres/Commands/Process.cs
@@ -60,7 +60,7 @@ namespace EXBP.Dipren.Demo.Postgres.Commands
 
             CuboidDataSource source = new CuboidDataSource(connectionString);
             CubiodBatchProcessor processor = new CubiodBatchProcessor(connectionString);
-            Job<Guid, Cuboid> job = new Job<Guid, Cuboid>(name, source, GuidKeyArithmetics.LexicographicalOrder, GuidKeySerializer.Default, processor, batchTimeout, batchSize);
+            Job<Guid, Cuboid> job = new Job<Guid, Cuboid>(name, source, GuidKeyArithmetics.LexicographicalMemberwise, GuidKeySerializer.Default, processor, batchTimeout, batchSize);
 
             await engine.RunAsync(job, false);
         }
