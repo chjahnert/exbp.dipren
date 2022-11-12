bba9233c7dc8f1ed7f3058f1447f2f3bfb5773e0
Christian Jähnert <16286828+chjahnert@users.noreply.github.com>
2022-11-12 17:45:48 +0100

    Improves the usability of the batch scripts for running the Postgres Demo application (#46)

--- demo/run postgres demo ascending.cmd
+++ demo/run postgres demo ascending.cmd
@@ -1,5 +1,7 @@
 @dotnet build --debug .\EXBP.Dipren.Demo.Postgres\EXBP.Dipren.Demo.Postgres.csproj
 
-@.\EXBP.Dipren.Demo.Postgres\bin\Debug\net6.0\EXBP.Dipren.Demo.Postgres.exe deploy --database "Host = localhost; Port = 5432; Database = postgres; User ID = postgres; Password = development" --size 100000
-@.\EXBP.Dipren.Demo.Postgres\bin\Debug\net6.0\EXBP.Dipren.Demo.Postgres.exe schedule "cuboid-001" --database "Host = localhost; Port = 5432; Database = postgres; User ID = postgres; Password = development"
-@.\EXBP.Dipren.Demo.Postgres\bin\Debug\net6.0\EXBP.Dipren.Demo.Postgres.exe process "cuboid-001" --database "Host = localhost; Port = 5432; Database = postgres; User ID = postgres; Password = development" --threads 7 --batch-size 128 --batch-timeout 00:00:02 --clock-drift 00:00:00
+@set database="Host = localhost; Port = 5432; Database = postgres; User ID = postgres; Password = development"
+
+@.\EXBP.Dipren.Demo.Postgres\bin\Debug\net6.0\EXBP.Dipren.Demo.Postgres.exe deploy --database %database% --size 100000
+@.\EXBP.Dipren.Demo.Postgres\bin\Debug\net6.0\EXBP.Dipren.Demo.Postgres.exe schedule "cuboid-001" --database %database%
+@.\EXBP.Dipren.Demo.Postgres\bin\Debug\net6.0\EXBP.Dipren.Demo.Postgres.exe process "cuboid-001" --database %database% --threads 7 --batch-size 128 --batch-timeout 00:00:02 --clock-drift 00:00:00
