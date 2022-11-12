92d6bfb639569ac3dd17f8b30e48d78d7efdd98b
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-12 15:51:55 +0100

    Handles the error that may occur if a batch is processed more than once.

--- demo/EXBP.Dipren.Demo.Postgres/Processing/CubiodBatchProcessor.cs
+++ demo/EXBP.Dipren.Demo.Postgres/Processing/CubiodBatchProcessor.cs
@@ -11,6 +11,8 @@ namespace EXBP.Dipren.Demo.Postgres.Processing
 {
     internal class CubiodBatchProcessor : IBatchProcessor<Cuboid>
     {
+        private const string SQL_STATE_PRIMARY_KEY_VIOLATION = "23505";
+
         private readonly string _connectionString;
 
         internal CubiodBatchProcessor(string connectionString)
@@ -35,7 +37,20 @@ namespace EXBP.Dipren.Demo.Postgres.Processing
                         await writer.WriteAsync((cuboid.Width * cuboid.Height * cuboid.Depth), NpgsqlDbType.Integer, cancellation);
                     }
 
-                    await writer.CompleteAsync();
+                    try
+                    {
+                        await writer.CompleteAsync();
+                    }
+                    catch (PostgresException ex) when (ex.SqlState == SQL_STATE_PRIMARY_KEY_VIOLATION)
+                    {
+                        //
+                        // In case a processing node crashed or was terminated before it had a chance to register the
+                        // progress it has made, the last batch may be reprocessed in which case we could end up with
+                        // a primary key violation error.
+                        //
+
+                        Debug.WriteLine("PRIMARY KEY VIOLATION");
+                    }
                 }
             }
         }
