1d2965439f3168e77ac545503eea55f786850afd
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-18 00:28:00 +0100

    Introduces the MovingAverage type.

---
+++ source/EXBP.Dipren.Tests/Telemetry/MovingAverageTests.cs
@@ -0,0 +1,80 @@
+﻿
+using EXBP.Dipren.Telemetry;
+
+using NUnit.Framework;
+
+
+namespace EXBP.Dipren.Tests.Telemetry
+{
+    [TestFixture]
+    public class MovingAverageTests
+    {
+        [Test]
+        public void AverageProperty_Empty_ReturnsCorrectAverage()
+        {
+            MovingAverage ma = new MovingAverage(4);
+
+            Assert.That(ma.Average, Is.Zero);
+        }
+
+        [Test]
+        public void AverageProperty_Partial_ReturnsCorrectAverage()
+        {
+            MovingAverage ma = new MovingAverage(4);
+
+            ma.Add(3D);
+            ma.Add(5D);
+
+            Assert.That(ma.Average, Is.EqualTo(4D));
+        }
+
+        [Test]
+        public void AverageProperty_Saturated_ReturnsCorrectAverage()
+        {
+            MovingAverage ma = new MovingAverage(4);
+
+            ma.Add(1D);
+
+            ma.Add(2D);
+            ma.Add(5D);
+            ma.Add(4D);
+            ma.Add(5D);
+
+            Assert.That(ma.Average, Is.EqualTo(4D));
+        }
+
+        [Test]
+        public void CountProperty_Empty_ReturnsCorrectCount()
+        {
+            MovingAverage ma = new MovingAverage(4);
+
+            Assert.That(ma.Count, Is.Zero);
+        }
+
+        [Test]
+        public void CountProperty_Partial_ReturnsCorrectCount()
+        {
+            MovingAverage ma = new MovingAverage(4);
+
+            ma.Add(3D);
+            ma.Add(5D);
+
+            Assert.That(ma.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CountProperty_Saturated_ReturnsCorrectCount()
+        {
+            MovingAverage ma = new MovingAverage(4);
+
+            ma.Add(1D);
+
+            ma.Add(2D);
+            ma.Add(5D);
+            ma.Add(4D);
+            ma.Add(5D);
+
+            Assert.That(ma.Count, Is.EqualTo(4));
+        }
+    }
+}
---
+++ source/EXBP.Dipren/Telemetry/MovingAverage.cs
@@ -0,0 +1,86 @@
+﻿
+namespace EXBP.Dipren.Telemetry
+{
+    /// <summary>
+    ///   Calculates a moving average.
+    /// </summary>
+    internal class MovingAverage
+    {
+        private readonly double[] _values;
+        private int _position = 0;
+        private bool _saturated = false;
+
+
+        /// <summary>
+        ///   Gets the number of values contained in the current instance.
+        /// </summary>
+        /// <value>
+        ///   A <see cref="int"/> value that contains the number of values contained in the current instance.
+        /// </value>
+        public int Count => (this._saturated == true) ? this._values.Length : this._position;
+
+
+        /// <summary>
+        ///   Gets the average of the contained values.
+        /// </summary>
+        /// <value>
+        ///   A <see cref="double"/> value that contains the average value of the contained values.
+        /// </value>
+        public double Average
+        {
+            get
+            {
+                double result = 0D;
+
+                if (this._saturated == true)
+                {
+                    result = this._values.Average();
+                }
+                else
+                {
+                    if (this._position > 0)
+                    {
+                        result = this._values.Take(this._position).Average();
+                    }
+                }
+
+                return result;
+            }
+        }
+
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="MovingAverage"/> class.
+        /// </summary>
+        /// <param name="size">
+        ///   The number of values to use to calculate the average.
+        /// </param>
+        public MovingAverage(int size)
+        {
+            this._values = new double[size];
+        }
+
+
+        /// <summary>
+        ///   Adds a new value to the set of values.
+        /// </summary>
+        /// <param name="value">
+        ///   The value to add.
+        /// </param>
+        /// <remarks>
+        ///   If the 
+        /// </remarks>
+        public void Add(double value)
+        {
+            this._values[this._position] = value;
+
+            this._position += 1;
+
+            if (this._position >= this._values.Length)
+            {
+                this._position = 0;
+                this._saturated = true;
+            }
+        }
+    }
+}
