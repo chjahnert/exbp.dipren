8dd3b25d266c07431f56dd9eb966378811dde34d
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-20 00:10:39 +0100

    Introduces the BackoffRetryPolicy type.

---
+++ source/EXBP.Dipren.Tests/Resilience/BackoffRetryStrategyTests.cs
@@ -0,0 +1,73 @@
+﻿
+using NUnit.Framework;
+
+using EXBP.Dipren.Resilience;
+
+
+namespace EXBP.Dipren.Tests.Resilience
+{
+    [TestFixture]
+    public class BackoffRetryStrategyTests
+    {
+        [Test]
+        public async Task Execute_PermanentErrorOccurrs_NoRetryIsAttempted()
+        {
+            BackoffRetryPolicy policy = new BackoffRetryPolicy(1, (attempt, exception) => TimeSpan.Zero, (attempt, exception) => false);
+
+            int attempts = 0;
+            int thrown = 0;
+
+            try
+            {
+                await policy.ExecuteAsync(() => {
+                   attempts += 1;
+                   throw new Exception("Permanent error condition.");
+                });
+            }
+            catch
+            {
+                thrown += 1;
+            }
+
+            Assert.That(attempts, Is.EqualTo(1));
+            Assert.That(thrown, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Execute_TransientErrorOccurrs_RetryIsAttempted()
+        {
+            BackoffRetryPolicy policy = new BackoffRetryPolicy(3, (attempt, exception) => TimeSpan.Zero, (attempt, exception) => true);
+
+            int attempts = 0;
+
+            Assert.ThrowsAsync<Exception>(async () => {
+                await policy.ExecuteAsync(() => {
+                    attempts += 1;
+                    throw new Exception("Transient error condition.");
+                });
+            });
+
+            Assert.That(attempts, Is.EqualTo(4));
+        }
+
+        [Test]
+        public async Task Execute_TransientErrorOccurrs_StopsAfterSuccessfulAttempts()
+        {
+            BackoffRetryPolicy policy = new BackoffRetryPolicy(3, (attempt, exception) => TimeSpan.Zero, (attempt, exception) => true);
+
+            int executions = 0;
+
+            int attempts = await policy.ExecuteAsync(() => {
+
+               executions += 1;
+
+               if (executions < 2)
+               {
+                   throw new Exception("Transient error condition.");
+               }
+            });
+
+            Assert.That(attempts, Is.EqualTo(2));
+        }
+    }
+}
---
+++ source/EXBP.Dipren/Resilience/BackoffRetryPolicy.cs
@@ -0,0 +1,240 @@
+﻿
+using EXBP.Dipren.Diagnostics;
+
+
+namespace EXBP.Dipren.Resilience
+{
+    /// <summary>
+    ///   Implements a retry policy that is based on delays between retry attempts.
+    /// </summary>
+    public class BackoffRetryPolicy
+    {
+        private readonly int _attempts;
+        private readonly Func<int, Exception, TimeSpan> _getRetryDelay;
+        private readonly Func<int, Exception, bool> _isTransientError;
+
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref="BackoffRetryPolicy"/> class.
+        /// </summary>
+        /// <param name="retries">
+        ///   The total number of retry attempts to make before failing the operation.
+        /// </param>
+        /// <param name="getRetryDelay">
+        ///   A <see cref="Func{T1, T2, TResult}"/> that returns the time to wait before retry attempts.
+        /// </param>
+        /// <param name="isTransientError">
+        ///   A <see cref="Func{T1, T2, TResult}"/> object that determines whether an exception represents a transient
+        ///   error condition.
+        /// </param>
+        /// <exception cref="ArgumentException">
+        ///   Argument <
+        /// </exception>
+        public BackoffRetryPolicy(int retries, Func<int, Exception, TimeSpan> getRetryDelay, Func<int, Exception, bool> isTransientError)
+        {
+            Assert.ArgumentIsGreaterOrEqual(retries, 0, nameof(retries));
+            Assert.ArgumentIsNotNull(getRetryDelay, nameof(getRetryDelay));
+            Assert.ArgumentIsNotNull(isTransientError, nameof(isTransientError));
+
+            this._attempts = (1 + retries);
+            this._getRetryDelay = getRetryDelay;
+            this._isTransientError = isTransientError;
+        }
+
+        /// <summary>
+        ///   Executes the specified action.
+        /// </summary>
+        /// <param name="action">
+        ///   The action to be executed.
+        /// </param>
+        /// <returns>
+        ///   An <see cref="int"/> value that indicates the total number of attempts performed.
+        /// </returns>
+        public int Execute(Action action)
+        {
+            Assert.ArgumentIsNotNull(action, nameof(action));
+
+            bool retry = false;
+            int result = 0;
+
+            do
+            {
+                retry = false;
+                result += 1;
+
+                try
+                {
+                    action.Invoke();
+                }
+                catch (Exception exception)
+                {
+                    if (result < this._attempts)
+                    {
+                        retry = this._isTransientError.Invoke(result, exception);
+
+                        if (retry == true)
+                        {
+                            TimeSpan duration = this._getRetryDelay.Invoke(result, exception);
+
+                            if (duration >= TimeSpan.Zero)
+                            {
+                                Thread.Sleep(duration);
+                            }
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            while (retry == true);
+
+            return result;
+        }
+
+        /// <summary>
+        ///   Runs the specified action.
+        /// </summary>
+        /// <param name="action">
+        ///   The action to be executed.
+        /// </param>
+        /// <returns>
+        ///   An <see cref="int"/> value that indicates the total number of execution attempts performed.
+        /// </returns>
+        public async Task<int> ExecuteAsync(Action action) => await this.ExecuteAsync(action, CancellationToken.None);
+
+        /// <summary>
+        ///   Runs the specified action.
+        /// </summary>
+        /// <param name="action">
+        ///   The action to be executed.
+        /// </param>
+        /// <param name="cancellation">
+        ///   A <see cref="CancellationToken"/> value that can be used to cancel the operation.
+        /// </param>
+        /// <returns>
+        ///   An <see cref="int"/> value that indicates the total number of execution attempts performed.
+        /// </returns>
+        public async Task<int> ExecuteAsync(Action action, CancellationToken cancellation)
+        {
+            Assert.ArgumentIsNotNull(action, nameof(action));
+
+            bool retry = false;
+            int result = 0;
+
+            do
+            {
+                retry = false;
+                result += 1;
+
+                try
+                {
+                    action.Invoke();
+                }
+                catch (Exception exception)
+                {
+                    if (result < this._attempts)
+                    {
+                        retry = this._isTransientError.Invoke(result, exception);
+
+                        if (retry == true)
+                        {
+                            TimeSpan duration = this._getRetryDelay.Invoke(result, exception);
+
+                            if (duration >= TimeSpan.Zero)
+                            {
+                                await Task.Delay(duration, cancellation);
+                            }
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            while (retry == true);
+
+            return result;
+        }
+
+        /// <summary>
+        ///   Runs the specified asynchronous method.
+        /// </summary>
+        /// <param name="action">
+        ///   The asynchronous action to be executed.
+        /// </param>
+        /// <returns>
+        ///   An <see cref="int"/> value that indicates the total number of execution attempts performed.
+        /// </returns>
+        public async Task<int> ExecuteAsync(Func<Task> action) => await this.ExecuteAsync(action, CancellationToken.None);
+
+        /// <summary>
+        ///   Runs the specified asynchronous method.
+        /// </summary>
+        /// <param name="action">
+        ///   The asynchronous action to be executed.
+        /// </param>
+        /// <param name="cancellation">
+        ///   A <see cref="CancellationToken"/> value that can be used to cancel the operation.
+        /// </param>
+        /// <returns>
+        ///   An <see cref="int"/> value that indicates the total number of execution attempts performed.
+        /// </returns>
+        public async Task<int> ExecuteAsync(Func<Task> action, CancellationToken cancellation)
+        {
+            Assert.ArgumentIsNotNull(action, nameof(action));
+
+            bool retry = false;
+            int result = 0;
+
+            do
+            {
+                retry = false;
+                result += 1;
+
+                try
+                {
+                    await action.Invoke();
+                }
+                catch (Exception exception)
+                {
+                    if (result < this._attempts)
+                    {
+                        retry = this._isTransientError.Invoke(result, exception);
+
+                        if (retry == true)
+                        {
+                            TimeSpan duration = this._getRetryDelay.Invoke(result, exception);
+
+                            if (duration >= TimeSpan.Zero)
+                            {
+                                await Task.Delay(duration, cancellation);
+                            }
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            while (retry == true);
+
+            return result;
+        }
+    }
+}
