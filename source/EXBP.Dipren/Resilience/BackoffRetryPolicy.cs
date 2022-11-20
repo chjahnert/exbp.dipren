0932094f14f1fadee767933cb59f6d7397b13d00
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-20 23:47:27 +0100

    Updates XML documentation comments on the BackoffRetryPolicy type and its members.

--- source/EXBP.Dipren/Resilience/BackoffRetryPolicy.cs
+++ source/EXBP.Dipren/Resilience/BackoffRetryPolicy.cs
@@ -5,49 +5,56 @@ using EXBP.Dipren.Diagnostics;
 namespace EXBP.Dipren.Resilience
 {
     /// <summary>
-    ///   Implements a retry policy that is based on delays between retry attempts.
+    ///   Implements a retry policy that waits some time before retry attempts.
     /// </summary>
     internal class BackoffRetryPolicy
     {
         private readonly int _attempts;
-        private readonly Func<int, TimeSpan> _getRetryDelay;
-        private readonly Func<Exception, bool> _isTransientError;
+        private readonly Func<int, TimeSpan> _getRetryDelayFunction;
+        private readonly Func<Exception, bool> _isTransientErrorFunction;
 
 
         /// <summary>
         ///   Initializes a new instance of the <see cref="BackoffRetryPolicy"/> class.
         /// </summary>
-        /// <param name="retries">
-        ///   The total number of retry attempts to make before failing the operation.
+        /// <param name="retryAttempts">
+        ///   The total number of retry attempts to make.
         /// </param>
         /// <param name="getRetryDelay">
-        ///   A <see cref="Func{T1, T2, TResult}"/> that returns the time to wait before retry attempts.
+        ///   A function that returns the time to wait before retry attempts.
         /// </param>
         /// <param name="isTransientError">
-        ///   A <see cref="Func{T1, T2, TResult}"/> object that determines whether an exception represents a transient
-        ///   error condition.
+        ///   A function that determines whether an exception represents a transient error condition.
         /// </param>
         /// <exception cref="ArgumentException">
-        ///   Argument <
+        ///   Argument <paramref name="retryAttempts"/> is less than zero.
         /// </exception>
-        public BackoffRetryPolicy(int retries, Func<int, TimeSpan> getRetryDelay, Func<Exception, bool> isTransientError)
+        /// <exception cref="ArgumentNullException">
+        ///   Argument <paramref name="getRetryDelay"/> or <paramref name="isTransientError"/> is a
+        ///   <see langword="null"/> reference.
+        /// </exception>
+        public BackoffRetryPolicy(int retryAttempts, Func<int, TimeSpan> getRetryDelay, Func<Exception, bool> isTransientError)
         {
-            Assert.ArgumentIsGreaterOrEqual(retries, 0, nameof(retries));
+            Assert.ArgumentIsGreaterOrEqual(retryAttempts, 0, nameof(retryAttempts));
             Assert.ArgumentIsNotNull(getRetryDelay, nameof(getRetryDelay));
             Assert.ArgumentIsNotNull(isTransientError, nameof(isTransientError));
 
-            this._attempts = (1 + retries);
-            this._getRetryDelay = getRetryDelay;
-            this._isTransientError = isTransientError;
+            this._attempts = (1 + retryAttempts);
+            this._getRetryDelayFunction = getRetryDelay;
+            this._isTransientErrorFunction = isTransientError;
         }
 
 
         /// <summary>
-        ///   Executes the specified action.
+        ///   Executes the specified action. If the action fails with a transient error, it is retried up to the
+        ///   number of times at construction time.
         /// </summary>
         /// <param name="action">
         ///   The action to be executed.
         /// </param>
+        /// <exception cref="ArgumentNullException">
+        ///   Argument <paramref name="action"/> is a <see langword="null"/> reference.
+        /// </exception>
         public void Execute(Action action)
         {
             Assert.ArgumentIsNotNull(action, nameof(action));
@@ -68,11 +75,11 @@ namespace EXBP.Dipren.Resilience
                 {
                     if (attempt < this._attempts)
                     {
-                        retry = this._isTransientError.Invoke(exception);
+                        retry = this._isTransientErrorFunction.Invoke(exception);
 
                         if (retry == true)
                         {
-                            TimeSpan duration = this._getRetryDelay.Invoke(attempt);
+                            TimeSpan duration = this._getRetryDelayFunction.Invoke(attempt);
 
                             if (duration >= TimeSpan.Zero)
                             {
@@ -94,7 +101,8 @@ namespace EXBP.Dipren.Resilience
         }
 
         /// <summary>
-        ///   Executes the specified action.
+        ///   Executes the specified action. If the action fails with a transient error, it is retried up to the
+        ///   number of times at construction time.
         /// </summary>
         /// <param name="action">
         ///   The action to be executed.
@@ -105,6 +113,13 @@ namespace EXBP.Dipren.Resilience
         /// <returns>
         ///   A <see cref="Task"/> object that represents the asynchronous operation.
         /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   Argument <paramref name="action"/> is a <see langword="null"/> reference.
+        /// </exception>
+        /// <remarks>
+        ///   The specified action is executed in a synchronous fashion, but the waiting between retry attempt is
+        ///   asynchronous.
+        /// </remarks>
         public async Task ExecuteAsync(Action action, CancellationToken cancellation = default)
         {
             Assert.ArgumentIsNotNull(action, nameof(action));
@@ -125,11 +140,11 @@ namespace EXBP.Dipren.Resilience
                 {
                     if (attempt < this._attempts)
                     {
-                        retry = this._isTransientError.Invoke(exception);
+                        retry = this._isTransientErrorFunction.Invoke(exception);
 
                         if (retry == true)
                         {
-                            TimeSpan duration = this._getRetryDelay.Invoke(attempt);
+                            TimeSpan duration = this._getRetryDelayFunction.Invoke(attempt);
 
                             if (duration >= TimeSpan.Zero)
                             {
@@ -151,17 +166,24 @@ namespace EXBP.Dipren.Resilience
         }
 
         /// <summary>
-        ///   Runs the specified asynchronous method.
+        ///   Executes the specified asynchronous action. If the action fails with a transient error, it is retried up
+        ///   to the number of times at construction time.
         /// </summary>
         /// <param name="action">
-        ///   The asynchronous action to be executed.
+        ///   The asynchronous action to execute.
         /// </param>
         /// <param name="cancellation">
         ///   A <see cref="CancellationToken"/> value that can be used to cancel the operation.
         /// </param>
         /// <returns>
-        ///   An <see cref="int"/> value that indicates the total number of execution attempts performed.
+        ///   A <see cref="Task"/> object that represents the asynchronous operation.
         /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///   Argument <paramref name="action"/> is a <see langword="null"/> reference.
+        /// </exception>
+        /// <remarks>
+        ///   Both the specified action and the waiting between retry attempt is run in an asynchronous fashion.
+        /// </remarks>
         public async Task ExecuteAsync(Func<Task> action, CancellationToken cancellation = default)
         {
             Assert.ArgumentIsNotNull(action, nameof(action));
@@ -182,11 +204,11 @@ namespace EXBP.Dipren.Resilience
                 {
                     if (attempt < this._attempts)
                     {
-                        retry = this._isTransientError.Invoke(exception);
+                        retry = this._isTransientErrorFunction.Invoke(exception);
 
                         if (retry == true)
                         {
-                            TimeSpan duration = this._getRetryDelay.Invoke(attempt);
+                            TimeSpan duration = this._getRetryDelayFunction.Invoke(attempt);
 
                             if (duration >= TimeSpan.Zero)
                             {
@@ -208,20 +230,30 @@ namespace EXBP.Dipren.Resilience
         }
 
         /// <summary>
-        ///   Runs the specified asynchronous method.
+        ///   Executes the specified asynchronous function. If the function fails with a transient error, it is retried
+        ///   up to the number of times at construction time.
         /// </summary>
-        /// <param name="action">
-        ///   The asynchronous action to be executed.
+        /// <typeparam name="TResult">
+        ///   The type the asynchronous function returns.
+        /// </typeparam>
+        /// <param name="function">
+        ///   The asynchronous function to execute.
         /// </param>
         /// <param name="cancellation">
         ///   A <see cref="CancellationToken"/> value that can be used to cancel the operation.
         /// </param>
         /// <returns>
-        ///   An <see cref="int"/> value that indicates the total number of execution attempts performed.
+        ///   A <see cref="Task{TResult}"/> object that represents the asynchronous operation.
         /// </returns>
-        public async Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> action, CancellationToken cancellation = default)
+        /// <exception cref="ArgumentNullException">
+        ///   Argument <paramref name="function"/> is a <see langword="null"/> reference.
+        /// </exception>
+        /// <remarks>
+        ///   Both the specified action and the waiting between retry attempt is run in an asynchronous fashion.
+        /// </remarks>
+        public async Task<TResult> ExecuteAsync<TResult>(Func<Task<TResult>> function, CancellationToken cancellation = default)
         {
-            Assert.ArgumentIsNotNull(action, nameof(action));
+            Assert.ArgumentIsNotNull(function, nameof(function));
 
             bool retry = false;
             int attempt = 0;
@@ -234,17 +266,17 @@ namespace EXBP.Dipren.Resilience
 
                 try
                 {
-                    result = await action.Invoke();
+                    result = await function.Invoke();
                 }
                 catch (Exception exception)
                 {
                     if (attempt < this._attempts)
                     {
-                        retry = this._isTransientError.Invoke(exception);
+                        retry = this._isTransientErrorFunction.Invoke(exception);
 
                         if (retry == true)
                         {
-                            TimeSpan duration = this._getRetryDelay.Invoke(attempt);
+                            TimeSpan duration = this._getRetryDelayFunction.Invoke(attempt);
 
                             if (duration >= TimeSpan.Zero)
                             {
