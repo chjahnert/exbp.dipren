a4cf5c9da05208427c3b824683945a50608eecef
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-21 09:08:10 +0100

    Applies code formatting.

--- source/Store/Postgres/EXBP.Dipren.Data.Postgres.Tests/ResilientPostgresEngineDataStoreTests.cs
+++ source/Store/Postgres/EXBP.Dipren.Data.Postgres.Tests/ResilientPostgresEngineDataStoreTests.cs
@@ -10,7 +10,9 @@ namespace EXBP.Dipren.Data.Postgres.Tests
         private const int DEFAULT_RETRY_LIMIT = 0;
         private const int DEFAULT_RETRY_DELAY = 20;
 
+
         private TimeSpan DefaultRetryDely => TimeSpan.FromMilliseconds(DEFAULT_RETRY_DELAY);
+
 
         protected override Task<IEngineDataStore> OnCreateEngineDataStoreAsync()
             => Task.FromResult<IEngineDataStore>(new ResilientPostgresEngineDataStore(CONNECTION_STRING, DEFAULT_RETRY_LIMIT, this.DefaultRetryDely));
