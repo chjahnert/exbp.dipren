daa45084304e9f9d3d4d00fd7f5e274cead93ce6
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-24 13:57:16 +0100

    Fixes the ExponentialBackoffDelyaProviderTests.GetDelay_ArgumentAttemptIsValid_ReturnsCorrectDelay(...) test.

--- source/EXBP.Dipren.Tests/Resilience/ExponentialBackoffDelyaProviderTests.cs
+++ source/EXBP.Dipren.Tests/Resilience/ExponentialBackoffDelyaProviderTests.cs
@@ -14,14 +14,18 @@ namespace EXBP.Dipren.Tests.Resilience
         [TestCase(1, 3, 4)]
         [TestCase(1, 4, 8)]
         [TestCase(1, 5, 16)]
+        [TestCase(3, 1, 3)]
+        [TestCase(3, 3, 12)]
+        [TestCase(3, 5, 48)]
         public void GetDelay_ArgumentAttemptIsValid_ReturnsCorrectDelay(int initial, int attempt, int expected)
         {
             TimeSpan delay = TimeSpan.FromMilliseconds(initial);
             ExponentialBackoffDelayProvider provider = new ExponentialBackoffDelayProvider(delay);
 
             TimeSpan result = provider.GetDelay(attempt);
+            TimeSpan correct = TimeSpan.FromMilliseconds(expected);
 
-            Assert.That(result, Is.EqualTo(delay * expected));
+            Assert.That(result, Is.EqualTo(correct));
         }
 
         [Test]
