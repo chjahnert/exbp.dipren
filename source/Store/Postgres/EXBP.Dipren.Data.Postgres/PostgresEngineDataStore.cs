1c15be5129fda816f4782fb0dbfef1b0bb0c6634
Christian Jähnert <ch.jahnert@gmail.com>
2022-11-25 12:03:00 +0100

    Updates the XML documentation comments on the PostgresEngineDataStore type.

--- source/Store/Postgres/EXBP.Dipren.Data.Postgres/PostgresEngineDataStore.cs
+++ source/Store/Postgres/EXBP.Dipren.Data.Postgres/PostgresEngineDataStore.cs
@@ -6,11 +6,29 @@ using EXBP.Dipren.Resilience;
 namespace EXBP.Dipren.Data.Postgres
 {
     /// <summary>
-    ///   Implements an <see cref="IEngineDataStore"/> that uses Postgres SQL as its storage engine and uses a backoff
-    ///   retry policy for resilience.
+    ///   Implements an <see cref="IEngineDataStore"/> that uses Postgres SQL as its storage engine. and uses a backoff
+    ///   retry policy for resilience. The default retry strategy uses an exponential backoff delay.
     /// </summary>
     /// <remarks>
-    ///   The implementation assumes that the required schema and table structure is already deployed.
+    ///   <para>
+    ///     The <see cref="PostgresEngineDataStore"/> implements a retry mechanism for resilience against transient
+    ///     errors. The built in retry mechanism uses an exponential backoff retry strategy that retries operations
+    ///     failing due to transient errors up to 12 times with exponentially growing wait times between each attempt.
+    ///     It starts with 5 milliseconds, then 10, 20, 40, 80, and so on up to a maximum of 12 retry attempts. This
+    ///     adds up to about 20 seconds in total before the operation is failed permanently.
+    ///   </para>
+    ///   <para>
+    ///     In case the built in retry mechanism does not meet your needs, you can inject you can implement your own
+    ///     retry strategy either from scratch or use an external framework such as
+    ///     <see href="http://www.thepollyproject.org/">The Polly Project</see>. An example is provided in the
+    ///     <see href="http://documentation">documentation</see>.
+    ///   </para>
+    ///   <para>
+    ///     The database schema has to be deployed before using this class.
+    ///   </para>
+    ///   <para>
+    ///     This class is thread-safe.
+    ///   </para>
     /// </remarks>
     public class PostgresEngineDataStore : ResilientEngineDataStore, IDisposable, IAsyncDisposable
     {
@@ -48,12 +66,8 @@ namespace EXBP.Dipren.Data.Postgres
         ///   The connection string to use when connecting to the database.
         /// </param>
         /// <param name="retryLimit">
-        ///   The number of retry attempts to perform in case a transient error occurs.
+        ///   The number of retry attempts to perform in case a transient error occurs. The default value is 12.
         /// </param>
-        /// <remarks>
-        ///   If the <paramref name="retryLimit"/> is not specified, the operations failing due to transient errors are
-        ///   retried up to 12 times, for about 20 seconds.
-        /// </remarks>
         public PostgresEngineDataStore(string connectionString, int retryLimit = DEFAULT_RETRY_LIMIT) : this(connectionString, retryLimit, TimeSpan.FromMilliseconds(DEFAULT_RETRY_DELAY))
         {
             TimeSpan retryDelay = TimeSpan.FromMilliseconds(DEFAULT_RETRY_DELAY);
