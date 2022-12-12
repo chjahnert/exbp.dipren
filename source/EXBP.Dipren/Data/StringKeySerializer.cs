259190e395ae820e532a0b43156c85b88886ce24
Christian Jähnert <16286828+chjahnert@users.noreply.github.com>
2022-12-13 00:28:19 +0100

    Adds support for serializing and deserializing string values (#82)

---
+++ source/EXBP.Dipren/Data/StringKeySerializer.cs
@@ -0,0 +1,44 @@
+﻿
+namespace EXBP.Dipren.Data
+{
+    /// <summary>
+    ///   Implements a default key serializer for the <see cref="string"/> type.
+    /// </summary>
+    /// <remarks>
+    ///   This type does not perform any changes to the values passed to it. It will always return the exact same value
+    ///   both during serialization and deserialization.
+    /// </remarks>
+    public sealed class StringKeySerializer : IKeySerializer<string>
+    {
+        /// <summary>
+        ///   A ready to be used instance of the <see cref="Int32KeySerializer"/> class.
+        /// </summary>
+        public static readonly StringKeySerializer Default = new StringKeySerializer();
+
+
+        /// <summary>
+        ///   Converts the specified string value to its serialized string representation.
+        /// </summary>
+        /// <param name="value">
+        ///   The value to convert to a <see cref="string"/>.
+        /// </param>
+        /// <returns>
+        ///   A <see cref="string"/> value containing the serialized representation of <paramref name="value"/>.
+        /// </returns>
+        public string Serialize(string value)
+            => value;
+
+        /// <summary>
+        ///   Converts the serialized representation of a <see cref="string"/> value to its original value.
+        /// </summary>
+        /// <param name="value">
+        ///   The serialized representation of the string value.
+        /// </param>
+        /// <returns>
+        ///   A <see cref="string"/> value converted from <paramref name="value"/>.
+        /// </returns>
+        public string Deserialize(string value)
+            => value;
+
+    }
+}
