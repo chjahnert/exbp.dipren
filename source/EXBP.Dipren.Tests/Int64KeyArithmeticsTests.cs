51b636a675d3d024cdbf853815a291bd539695df
Christian Jähnert <ch.jahnert@gmail.com>
2022-12-21 13:54:41 +0100

    Introduces the Int64KeyArithmetics.SplitAsync(...) method.

--- source/EXBP.Dipren.Tests/Int64KeyArithmeticsTests.cs
+++ source/EXBP.Dipren.Tests/Int64KeyArithmeticsTests.cs
@@ -55,5 +55,64 @@ namespace EXBP.Dipren.Tests
 
             Assert.That(created, Is.Null);
         }
+
+        [Test]
+        public void SplitAsync_ArgumentRangeIsNull_ThrowsException()
+        {
+            Assert.Throws<ArgumentNullException>(() => Int64KeyArithmetics.Default.SplitAsync(null, CancellationToken.None));
+        }
+
+        [TestCase(1, 8, true, 1, 5, 5, 8)]
+        [TestCase(8, 1, true, 8, 4, 4, 1)]
+        [TestCase(-7, 5, true, -7, -1, -1, 5)]
+        [TestCase(5, -7, true, 5, -1, -1, -7)]
+        [TestCase(1, 8, false, 1, 5, 5, 8)]
+        [TestCase(8, 1, false, 8, 4, 4, 1)]
+        [TestCase(-7, 5, false, -7, -1, -1, 5)]
+        [TestCase(5, -7, false, 5, -1, -1, -7)]
+        [TestCase(long.MinValue, long.MaxValue, false, long.MinValue, 0, 0, long.MaxValue)]
+        [TestCase(long.MaxValue, long.MinValue, false, long.MaxValue, -1, -1, long.MinValue)]
+        [TestCase(long.MinValue, long.MaxValue, true, long.MinValue, 0, 0, long.MaxValue)]
+        [TestCase(long.MaxValue, long.MinValue, true, long.MaxValue, -1, -1, long.MinValue)]
+        public async Task SplitAsync_ArgumentRangeIsSplittable_SplitsRangeCorrectly(long inputFirst, long inputLast, bool inputInclusive, long returnedFirst, long returnedLast, long createdFirst, long createdLast)
+        {
+            Range<long> input = new Range<long>(inputFirst, inputLast, inputInclusive);
+
+            RangePartitioningResult<long> result = await Int64KeyArithmetics.Default.SplitAsync(input, CancellationToken.None);
+
+            Assert.That(result, Is.Not.Null);
+
+            Assert.That(result.Updated.First, Is.EqualTo(returnedFirst));
+            Assert.That(result.Updated.Last, Is.EqualTo(returnedLast));
+            Assert.That(result.Updated.IsInclusive, Is.False);
+
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.Created.Count, Is.EqualTo(1));
+
+            Range<long> created = result.Created.First();
+
+            Assert.That(created.First, Is.EqualTo(createdFirst));
+            Assert.That(created.Last, Is.EqualTo(createdLast));
+            Assert.That(created.IsInclusive, Is.EqualTo(inputInclusive));
+        }
+
+        [TestCase(1, 1, true)]
+        [TestCase(1, 2, true)]
+        [TestCase(1, 3, false)]
+        public async Task SplitAsync_ArgumentRangeIsNotSplittable_ReturnUnchangedRange(long inputFirst, long inputLast, bool inputInclusive)
+        {
+            Range<long> input = new Range<long>(inputFirst, inputLast, inputInclusive);
+
+            RangePartitioningResult<long> result = await Int64KeyArithmetics.Default.SplitAsync(input, CancellationToken.None);
+
+            Assert.That(result, Is.Not.Null);
+
+            Assert.That(result.Updated.First, Is.EqualTo(input.First));
+            Assert.That(result.Updated.Last, Is.EqualTo(input.Last));
+            Assert.That(result.Updated.IsInclusive, Is.EqualTo(input.IsInclusive));
+
+            Assert.That(result.Success, Is.False);
+            Assert.That(result.Created, Is.Empty);
+        }
     }
 }
--- source/EXBP.Dipren/Int64KeyArithmetics.cs
+++ source/EXBP.Dipren/Int64KeyArithmetics.cs
@@ -74,5 +74,50 @@ namespace EXBP.Dipren
 
             return result;
         }
+
+        /// <summary>
+        ///   Splits the specified integer key range into two ranges.
+        /// </summary>
+        /// <param name="range">
+        ///   The <see cref="Range{TKey}"/> of <see cref="long"/> to split.
+        /// </param>
+        /// <param name="cancellation">
+        ///   The <see cref="CancellationToken"/> used to propagate notifications that the operation should be
+        ///   canceled.
+        /// </param>
+        /// <returns>
+        ///   A <see cref="Task{TResult}"/> object that represents the asynchronous operation.
+        /// </returns>
+        public Task<RangePartitioningResult<long>> SplitAsync(Range<long> range, CancellationToken cancellation)
+        {
+            Assert.ArgumentIsNotNull(range, nameof(range));
+
+            RangePartitioningResult<long> result;
+
+            double distance = Math.Abs(((double) range.Last) - ((double) range.First));
+
+            if (((range.IsInclusive == true) && (distance >= 2)) || ((range.IsInclusive == false) && (distance >= 3)))
+            {
+                long half = (long) Math.Round(distance / 2);
+
+                bool ascending = range.IsAscending(this._comparer);
+
+                if (ascending == false)
+                {
+                    half *= -1;
+                }
+
+                Range<long> updated = new Range<long>(range.First, range.First + half, false);
+                Range<long> created = new Range<long>(range.First + half, range.Last, range.IsInclusive);
+
+                result = new RangePartitioningResult<long>(updated, new Range<long>[] { created });
+            }
+            else
+            {
+                result = new RangePartitioningResult<long>(range, new Range<long>[0]);
+            }
+
+            return Task.FromResult(result);
+        }
     }
 }
