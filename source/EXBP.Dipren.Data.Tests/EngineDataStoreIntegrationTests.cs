6b8b687a91cac0d03c3231e01cbdd6f0cd74314d
Christian Jähnert <ch.jahnert@gmail.com>
2023-01-04 10:52:10 +0100

    Introduces the EngineDataStoreIntegrationTests.Cuboid type.

--- source/EXBP.Dipren.Data.Tests/EngineDataStoreIntegrationTests.cs
+++ source/EXBP.Dipren.Data.Tests/EngineDataStoreIntegrationTests.cs
@@ -1,7 +1,21 @@
 ﻿
+using System.Diagnostics;
+
+
 namespace EXBP.Dipren.Data.Tests
 {
     public abstract class EngineDataStoreIntegrationTests
     {
+        [DebuggerDisplay("ID = {Id}, Dimensions = {Width} × {Height} × {Depth} ")]
+        protected class Cuboid
+        {
+            public int Id { get; set; }
+
+            public float Width { get; set; }
+
+            public float Height { get; set; }
+
+            public float Depth { get; set; }
+        }
     }
 }
