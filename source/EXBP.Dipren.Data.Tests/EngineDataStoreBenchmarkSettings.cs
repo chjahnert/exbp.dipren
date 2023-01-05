6f6cd729ff1f5f9bf486893628e4ad3d422153d1
Christian Jähnert <16286828+chjahnert@users.noreply.github.com>
2023-01-05 13:40:18 +0100

    Adds a set of components that aid with performance optimization of engine data store implementations (#97)

---
+++ source/EXBP.Dipren.Data.Tests/EngineDataStoreBenchmarkSettings.cs
@@ -0,0 +1,50 @@
+﻿
+namespace EXBP.Dipren.Data.Tests
+{
+    public class EngineDataStoreBenchmarkSettings
+    {
+        private const int DEFAULT_PROCESSING_NODES = 13;
+        private const int DEFAULT_DATASET_SIZE = 100000;
+        private const int DEFAULT_BATCH_SIZE = 7;
+        private const int DEFAULT_PROCESSING_TIMEOUT = 1000;
+        private const int DEFAULT_POLLING_INTERVAL = 250;
+        private const int DEFAULT_REPORTING_INTERVAL = 100;
+        private const int DEFAULT_BATCH_PROCESSING_DURATION = 0;
+
+
+        public static EngineDataStoreBenchmarkSettings Small { get; } = new EngineDataStoreBenchmarkSettings
+        {
+            ProcessingNodes = 7,
+            DatasetsSize = 10000
+        };
+
+        public static EngineDataStoreBenchmarkSettings Medium { get; } = new EngineDataStoreBenchmarkSettings();
+
+        public static EngineDataStoreBenchmarkSettings Large { get; } = new EngineDataStoreBenchmarkSettings
+        {
+            ProcessingNodes = 47,
+            DatasetsSize = 1000000
+        };
+
+        public static EngineDataStoreBenchmarkSettings Huge { get; } = new EngineDataStoreBenchmarkSettings
+        {
+            ProcessingNodes = 63,
+            DatasetsSize = 10000000
+        };
+
+
+        public int ProcessingNodes { get; init; } = DEFAULT_PROCESSING_NODES;
+
+        public int DatasetsSize { get; init; } = DEFAULT_DATASET_SIZE;
+
+        public int BatchSize { get; init; } = DEFAULT_BATCH_SIZE;
+
+        public TimeSpan ProcessingTimeout { get; init; } = TimeSpan.FromMilliseconds(DEFAULT_PROCESSING_TIMEOUT);
+
+        public TimeSpan PollingInterval { get; init; } = TimeSpan.FromMilliseconds(DEFAULT_POLLING_INTERVAL);
+
+        public TimeSpan ReportingInterval { get; init; } = TimeSpan.FromMilliseconds(DEFAULT_REPORTING_INTERVAL);
+
+        public TimeSpan BatchProcessingDuration { get; init; } = TimeSpan.FromMilliseconds(DEFAULT_BATCH_PROCESSING_DURATION);
+    }
+}
---
+++ source/EXBP.Dipren.Data.Tests/IEngineDataStoreFactory.cs
@@ -0,0 +1,8 @@
+﻿
+namespace EXBP.Dipren.Data.Tests
+{
+    public interface IEngineDataStoreFactory
+    {
+        Task<IEngineDataStore> CreateAsync(CancellationToken cancellation);
+    }
+}
