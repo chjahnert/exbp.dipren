016c1fbbb6e1ba66ed31b4e68fd14809c25eb58d
Christian Jähnert <ch.jahnert@gmail.com>
2023-02-22 14:52:56 +0100

    Adds a unit tests covering the OpenTelemetryEngineMetrics.RegisterIsSplitRequestPending(...) method.

--- source/EXBP.Dipren.Tests/Telemetry/OpenTelemetryEngineMetricsTests.cs
+++ source/EXBP.Dipren.Tests/Telemetry/OpenTelemetryEngineMetricsTests.cs
@@ -368,5 +368,42 @@ namespace EXBP.Dipren.Tests.Telemetry
             Assert.That(count, Is.EqualTo(2));
             Assert.That(sum, Is.EqualTo(5));
         }
+
+        [Test]
+        public void RegisterIsSplitRequestPending_MultipleEventsRegistered_InstrumentsReflectEvents()
+        {
+            List<MetricSnapshot> metrics = new List<MetricSnapshot>();
+
+            using MeterProvider provider = Sdk.CreateMeterProviderBuilder()
+                .AddDiprenMeters()
+                .AddInMemoryExporter(metrics)
+                .Build();
+
+            string nodeId = "n1";
+            string jobId = "j1";
+            Guid partitionId = Guid.NewGuid();
+            TimeSpan duration1 = TimeSpan.FromMilliseconds(2);
+            TimeSpan duration2 = TimeSpan.FromMilliseconds(3);
+
+            OpenTelemetryEngineMetrics.Instance.RegisterIsSplitRequestPending(nodeId, jobId, duration1);
+            OpenTelemetryEngineMetrics.Instance.RegisterIsSplitRequestPending(nodeId, jobId, duration2);
+
+            provider.Shutdown();
+
+            MetricSnapshot snapshot = metrics.LastOrDefault(m => (m.Name == OpenTelemetryEngineMetrics.INSTRUMENT_NAME_IS_SPLIT_REQUEST_PENDING) && (m.MetricType == MetricType.Histogram));
+
+            Assert.That(snapshot, Is.Not.Null);
+            Assert.That(snapshot.MetricPoints.Count, Is.EqualTo(1));
+
+            MetricPoint point = snapshot.MetricPoints.FirstOrDefault(p =>
+                p.HasTag(OpenTelemetryEngineMetrics.TAG_NAME_NODE, nodeId) &&
+                p.HasTag(OpenTelemetryEngineMetrics.TAG_NAME_JOB, jobId));
+
+            long count = point.GetHistogramCount();
+            double sum = point.GetHistogramSum();
+
+            Assert.That(count, Is.EqualTo(2));
+            Assert.That(sum, Is.EqualTo(5));
+        }
     }
 }
